50573e91802d5bc1f10c6afa7d74528f7620622c
ginkohub <ginkohub@gmail.com>
2025-06-08 13:34:45 +0800

    add: pen for logging

---
+++ src/pen.js
@@ -0,0 +1,93 @@
+/**
+ * Copyright (C) 2025 Ginko
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/
+ *
+ * This code is part of Ginko project (https://github.com/ginkohub)
+ */
+
+export const LL_NONE = 0;
+export const LL_DEBUG = 1;
+export const LL_INFO = 2;
+export const LL_WARN = 3;
+export const LL_ERROR = 4;
+
+let TIME_FORMAT = 'HH:mm:ss.SSS';
+
+function getTime(format) {
+  if (!format || format === '') {
+    format = TIME_FORMAT;
+  }
+  const now = new Date();
+  const repl = {
+    'HH': now.getHours().toString().padStart(2, '0'),
+    'mm': now.getMinutes().toString().padStart(2, '0'),
+    'ss': now.getSeconds().toString().padStart(2, '0'),
+    'SSS': now.getMilliseconds().toString().padStart(3, '0')
+  }
+
+  for (const key in repl) {
+    format = format.replace(key, repl[key]);
+  }
+  return format;
+}
+
+export class Pen {
+
+  constructor(level) {
+    this.level = level;
+  }
+
+  Magenta(args) {
+    return '\x1b[35m' + args + '\x1b[0m';
+  }
+
+  Cyan(args) {
+    return '\x1b[36m' + args + '\x1b[0m';
+  }
+
+  Green(args) {
+    return '\x1b[32m' + args + '\x1b[0m';
+  }
+
+  Red(args) {
+    return '\x1b[31m' + args + '\x1b[0m';
+  }
+
+  Yellow(args) {
+    return '\x1b[33m' + args + '\x1b[0m';
+  }
+
+
+  Debug(args) {
+    if (this.level > LL_DEBUG || this.level == LL_NONE) {
+      return
+    }
+    console.log(`${getTime()} ${this.Magenta('[D]')}`, args);
+  }
+
+  Info(args) {
+    if (this.level > LL_INFO || this.level == LL_NONE) {
+      return
+    }
+    console.log(`${getTime()} ${this.Cyan('[I]')}`, args);
+  }
+
+  Warn(args) {
+    if (this.level > LL_WARN || this.level == LL_NONE) {
+      return
+    }
+    console.log(`${getTime()} ${this.Yellow('[W]')}`, args);
+  }
+
+  Error(args) {
+    if (this.level > LL_ERROR || this.level == LL_NONE) {
+      return
+    }
+    console.log(`${getTime()} ${this.Red('[E]')}`, args);
+  }
+}
+
+export default new Pen();
