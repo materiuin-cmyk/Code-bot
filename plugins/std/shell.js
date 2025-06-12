e2991032daf01c6ee00aef5236e1b1e709123b31
ginkohub <ginkohub@gmail.com>
2025-06-12 14:32:49 +0800

    feat: eval & shell

---
+++ plugins/std/shell.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2025 Ginko
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/
+ *
+ * This code is part of Ginko project (https://github.com/ginkohub)
+ */
+
+import { MESSAGES_UPSERT } from '../../src/const.js';
+import { eventNameIs, fromMe, midwareAnd } from '../../src/midware.js';
+import pen from '../../src/pen.js';
+import { exec } from 'child_process';
+
+/** @type {import('../../src/plugin.js').Plugin} */
+export default {
+  cmd: ['$'],
+  noPrefix: true,
+  timeout: 15,
+  cat: 'system',
+  tags: ['system'],
+  desc: 'Evaluate JavaScript code',
+
+  midware: midwareAnd(
+    eventNameIs(MESSAGES_UPSERT), fromMe,
+  ),
+
+  /** @param {import('../../src/context.js').Ctx} c */
+  exec: async (c) => {
+    pen.Warn(c.pattern, 'args :', c.args);
+    const src = c.args?.trim();
+    if (!src) return;
+
+    try {
+      /* Execute shell command */
+      exec(src, (error, stdout, stderr) => {
+        if (stderr) {
+          c.reply({ text: stderr.message });
+        }
+        if (stdout) {
+          c.reply({ text: stdout });
+        }
+      });
+    } catch (e) {
+      c.reply({ text: `${e}` });
+    }
+  }
+};
+
