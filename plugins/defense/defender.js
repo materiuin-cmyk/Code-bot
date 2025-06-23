91c81ae45a26c6a043ef9474d538254c1feb4fdd
ginkohub <ginkohub@gmail.com>
2025-06-24 00:16:54 +0800

    feat: defender auto block spam on status and lots jid on group

---
+++ plugins/defense/defender.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2025 Ginko
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/
+ *
+ * This code is part of Ginko project (https://github.com/ginkohub)
+ */
+
+import { MESSAGES_UPSERT } from '../../src/const.js';
+import { eventNameIs, midwareAnd, midwareOr } from '../../src/midware.js';
+import pen from '../../src/pen.js';
+
+const allowed = [
+  'extendedTextMessage',
+  'videoMessage',
+  'imageMessage',
+  'audioMessage'
+]
+
+/** @type {import('../../src/plugin.js').Plugin} */
+export default {
+  desc: 'Defense system',
+  midware: midwareAnd(
+    eventNameIs(MESSAGES_UPSERT),
+    midwareOr(
+      /** @param {import('../../src/context.js').Ctx} c */
+      (c) => { return c.isStatus && !allowed.includes(c.type) && !c.type },
+
+      /** @param {import('../../src/context.js').Ctx} c */
+      (c) => { return c.isGroup && c.mentionedJid?.length > 1024 }
+    )
+  ),
+
+  /** @param {import('../../src/context.js').Ctx} c */
+  exec: async (c) => {
+    pen.Warn('LogWatch', c.eventName, c.event);
+    try {
+      c.handler().client.sock.updateBlockStatus(c.sender, 'block');
+      if (c.isGroup && c.mentionedJid?.length > 1024) {
+        c.reply({ delete: c.key });
+      }
+    } catch (e) {
+      pen.Error(e);
+    }
+  }
+};
+
