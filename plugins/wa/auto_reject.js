60dbdd3df8ed405082567d009b539cfe99e5db95
ginkohub <ginkohub@gmail.com>
2025-06-27 08:15:20 +0800

    impv: auto reject using setting

--- plugins/wa/auto_reject.js
+++ plugins/wa/auto_reject.js
@@ -8,31 +8,65 @@
  * This code is part of Ginko project (https://github.com/ginkohub)
  */
 
-import { CALL } from '../../src/const.js';
-import { eventNameIs, midwareAnd } from '../../src/midware.js';
+import { CALL, MESSAGES_UPSERT } from '../../src/const.js';
+import { eventNameIs, fromMe, midwareAnd } from '../../src/midware.js';
 import pen from '../../src/pen.js';
 import { delay, randomNumber } from '../../src/tools.js';
+import { settings } from '../settings.js';
 
 
-/** @type {import('../../src/plugin.js').Plugin} */
-export default {
-  desc: 'Auto reject call',
-  timeout: 15,
+/** @type {import('../../src/plugin.js').Plugin[]} */
+export default [
+  {
+    desc: 'Auto reject call',
+    timeout: 15,
 
-  midware: midwareAnd(
-    eventNameIs(CALL),
-    (ctx) => !ctx.isStatus,
-    (ctx) => !ctx.fromMe,
-  ),
+    midware: midwareAnd(
+      eventNameIs(CALL),
+      (ctx) => !ctx.isStatus,
+      (ctx) => !ctx.fromMe,
+      (ctx) => settings.get(`auto_reject_${ctx.me}`)
+    ),
 
-  /** @param {import('../../src/context.js').Ctx} c */
-  exec: async (c) => {
-    if (c.callStatus !== 'offer') return;
+    /** @param {import('../../src/context.js').Ctx} c */
+    exec: async (c) => {
+      if (c.callStatus !== 'offer') return;
 
-    await delay(randomNumber(1000, 2000));
-    pen.Warn('Rejecting call from', c.senderName, c.sender);
+      await delay(randomNumber(1000, 2000));
+      pen.Warn('Rejecting call from', c.senderName, c.sender);
 
-    await c.handler().client.sock.rejectCall(c.id, c.sender);
+      await c.handler().client.sock.rejectCall(c.id, c.sender);
+    }
+  },
+
+  {
+    cmd: ['reject', 'reject+', 'reject-'],
+    cat: 'whatsapp',
+    desc: 'Set auto reject message',
+    timeout: 15,
+    midware: midwareAnd(
+      eventNameIs(MESSAGES_UPSERT),
+      fromMe,
+    ),
+    exec: async (c) => {
+      const key = `auto_reject_${c.me}`;
+      if (c.pattern.endsWith('+')) {
+        settings.set(key, true)
+        pen.Warn(`Activating auto reject for ${c.me}`);
+      } else if (c.pattern.endsWith('-')) {
+        settings.set(key, false)
+        pen.Warn(`Deactivating auto reject for ${c.me}`);
+      } else {
+        const set = settings.get(key);
+        let text = '';
+        if (set) {
+          text = `Auto reject status : *${set}*\n\nNB :\n  *${c.pattern}* _to deactivating_\n  *${c.pattern}+* _to activating_`
+        } else {
+          text = `Auto reject is not yet set.\n\nNB :\n  *${c.pattern}+* _to activating_`
+        }
+        c.reply({ text: text }, { qouted: c.message })
+      }
+    }
   }
-};
+]
 
