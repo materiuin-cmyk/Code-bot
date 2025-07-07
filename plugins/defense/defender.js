3c40eb99714d0c5f1a40f259fd292566d841b2ce
ginkohub <ginkohub@gmail.com>
2025-07-07 20:41:53 +0800

    feat: improves defense system detector and reasoning

--- plugins/defense/defender.js
+++ plugins/defense/defender.js
@@ -9,7 +9,7 @@
  */
 
 import { MESSAGES_UPSERT } from '../../src/const.js';
-import { eventNameIs, midwareAnd, midwareOr } from '../../src/midware.js';
+import { eventNameIs, fromMe, midwareAnd, midwareOr } from '../../src/midware.js';
 import pen from '../../src/pen.js';
 
 const allowed = [
@@ -23,34 +23,122 @@ const allowed = [
 
 let blockedUsers = [];
 
-/** @type {import('../../src/plugin.js').Plugin} */
-export default {
-  desc: 'Defense system',
-  midware: midwareAnd(
-    eventNameIs(MESSAGES_UPSERT),
-    midwareOr(
-      /** @param {import('../../src/context.js').Ctx} c */
-      (c) => { return c.isStatus && !allowed.includes(c.type) && c.type },
+class Result {
+  constructor({ suspect, reason, data }) {
+    this.suspect = suspect;
+    this.reason = reason;
+    this.data = data;
+  }
+}
 
-      /** @param {import('../../src/context.js').Ctx} c */
-      (c) => { return c.isGroup && c.mentionedJid?.length > 1024 }
-    )
-  ),
+class HitGen {
+  constructor(maxHit) {
+    this.key = '';
+    this.hits = 0;
+    this.maxHit = maxHit;
+  }
 
-  /** @param {import('../../src/context.js').Ctx} c */
-  exec: async (c) => {
-    pen.Warn('LogWatch', c.eventName, c.event);
-    try {
-      if (!blockedUsers.includes(c.sender)) {
-        c.handler().client.sock.updateBlockStatus(c.sender, 'block');
-        blockedUsers.push(c.sender);
-      }
-      if (c.isGroup && c.mentionedJid?.length > 1024) {
-        c.reply({ delete: c.key });
-      }
-    } catch (e) {
-      pen.Error(e);
+  isOverHit(key) {
+    if (this.key === key) {
+      this.hits++;
+    } else {
+      this.key = key;
+      this.hits = 1;
+    }
+    return this.hits >= this.maxHit;
+  }
+}
+
+const hitman = new HitGen(5);
+
+/** @typedef {(c: import('../../src/context.js').Ctx) => Result | any} Detector */
+/** @type {Detector[]} */
+const listDetectors = [
+  (c) => {
+    return {
+      suspect: c.isStatus && !allowed.includes(c.type) && c.type,
+      reason: 'Status message with not allowed type',
+      data: c,
+    };
+  },
+  (c) => {
+    return {
+      suspect: c.mentionedJid?.length > 1024,
+      reason: 'Too many mentioned jids',
+      data: c.contextInfo,
+    };
+  },
+  (c) => {
+    const key = `${c.chat}|${c.sender}|${c.timestamp}`;
+    return {
+      suspect: c.isViewOnce && hitman.isOverHit(key),
+      reason: 'Too many messages in a short time',
+      data: c,
     }
   }
-};
+];
+
+
+/** @type {import('../../src/plugin.js').Plugin[]} */
+export default [
+  {
+    desc: 'Defense system',
+    midware: midwareAnd(
+      eventNameIs(MESSAGES_UPSERT),
+    ),
+
+    /** @param {import('../../src/context.js').Ctx} c */
+    exec: async (c) => {
+      let detect = new Result({ suspect: false });
+      for (const detector of listDetectors) {
+        detect = new Result(detector(c));
+        if (detect?.suspect) {
+          break;
+        }
+      }
+
+      if (!detect.suspect) {
+        return;
+      }
+
+      pen.Warn('Defense :', c.eventName, detect.suspect, detect.reason, detect.data);
+      try {
+        if (!blockedUsers.includes(c.sender)) {
+          c.sendMessage(c.me, {
+            document: Buffer.from(JSON.stringify(detect, null, 2)),
+            fileName: `${c.chat}_${c.sender}_${c.timestamp}.json`,
+            mimetype: 'application/json',
+          });
+
+          c.handler().client.sock.updateBlockStatus(c.sender, 'block');
+          blockedUsers.push(c.sender);
+        }
+
+        if (c.isGroup && c.mentionedJid?.length > 1024) {
+          c.reply({ delete: c.key });
+        }
+      } catch (e) {
+        pen.Error(e);
+      }
+    }
+  },
+  {
+    cmd: ['smp'],
+    cat: 'defense',
+    timeout: 15,
+    midware: midwareAnd(
+      eventNameIs(MESSAGES_UPSERT),
+      midwareOr(fromMe),
+    ),
+
+    /** @param {import('../../src/context.js').Ctx} c */
+    exec: async (c) => {
+      c.reply({
+        document: Buffer.from(JSON.stringify(c)),
+        fileName: `${c.chat}_${c.sender}_${c.timestamp}.json`,
+        mimetype: 'application/json',
+      });
+    }
+  }
+]
 
