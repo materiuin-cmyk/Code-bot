b921173213d9848e2f29d0b09c7a3d2d02f3972a
ginkohub <ginkohub@gmail.com>
2025-07-14 13:17:35 +0800

    feat(tools): Add formatBytes utility function

--- src/tools.js
+++ src/tools.js
@@ -125,3 +125,16 @@ export function formatMD(s) {
   s = s.replace(reMono, '```$1```');
   return s;
 }
+
+/**
+ * Format bytes to human readable string
+ * @param {number} bytes
+ * @returns {string}
+ */
+export function formatBytes(bytes) {
+  if (bytes === 0) return '0 Bytes';
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+};
